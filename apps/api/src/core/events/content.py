52b660264a296897c86934c992d956d108385b36
Videsh Arivazhagan <arivvid27@gmail.com>
2025-10-20 18:29:39 -0700

    no more react

--- apps/api/src/core/events/content.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-async def check_content_directory():
-    if not os.path.exists("content"):
-        # create folder for activity
-        print("Creating content directory...")
-        os.makedirs("content")
--- apps/web/instrumentation.ts
+++
@@ -1,9 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    // Node.js specific instrumentation
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    // Edge runtime specific instrumentation
-  }
-}
